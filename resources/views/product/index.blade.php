b140f5a95f1147e4dfe0ed5aca243fe566f9c8a2
raihansatar <raihanroy25@gmail.com>
2021-02-06 00:21:39 +0800

    priiduct apage"

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,1 +1,32 @@
-index product
+
+
+@extends('layouts.main')
+
+@section('content')
+
+<div class="card card-custom">
+    <div class="card-header">
+        <div class="card-titlee">Product List</div>
+    </div>
+    <div class="card-body">
+        <div>
+            <table class="table">
+                <thead>
+                    <th>Product Name</th>
+                    <th>Buying Price</th>
+                    <th>Selling Price</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nasi Ayam</td>
+                        <td>Rm325253.325</td>
+                        <td>RMwerwer.werw</td>
+                        <td>Icon action sisni kei</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection

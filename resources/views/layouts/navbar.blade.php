1f12ddf767b55da353d9ecaf3c9f18ff1c174eeb
raihansatar <raihanroy25@gmail.com>
2021-02-18 23:40:50 +0800

    add request is for active navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,19 +12,19 @@
                         <a class="nav-link" aria-current="page" href="#">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ Route('sales.index') }}">Sales</a>
+                        <a class="nav-link {{ Request::is('sales')? 'active' : '' }}" aria-current="page" href="{{ Route('sales.index') }}">Sales</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ Route('product.index') }}">Product</a>
+                        <a class="nav-link {{ Request::is('product')? 'active' : '' }}" aria-current="page" href="{{ Route('product.index') }}">Product</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Shop</a>
+                        <a class="nav-link {{ Request::is('shop')? 'active' : '' }}" aria-current="page" href="#">Shop</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ Route('debt.index') }}">Debt</a>
+                        <a class="nav-link {{ Request::is('debt')? 'active' : '' }}" aria-current="page" href="{{ Route('debt.index') }}">Debt</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Report</a>
+                        <a class="nav-link {{ Request::is('report')? 'active' : '' }}" aria-current="page" href="#">Report</a>
                     </li>
             @endrole
                 </ul>

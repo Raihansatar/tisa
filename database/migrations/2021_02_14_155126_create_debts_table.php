5418f65baa4641cb11ffc236009a5dec4260f482
raihansatar <raihanroy25@gmail.com>
2021-02-21 01:05:57 +0800

    debt table

--- database/migrations/2021_02_14_155126_create_debts_table.php
+++ database/migrations/2021_02_14_155126_create_debts_table.php
@@ -15,6 +15,13 @@ class CreateDebtsTable extends Migration
     {
         Schema::create('debts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->decimal('debt', 10, 2);
+            $table->decimal('paid', 10, 2);
+            $table->text('note')->nullable();
+            $table->enum('status', ['unpaid', 'partial', 'paid']);
+            $table->softDeletes();
             $table->timestamps();
         });
     }

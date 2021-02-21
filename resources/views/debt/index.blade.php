cbb3799bd2bbb796ae35882e93d39f7fa856adc2
raihansatar <raihanroy25@gmail.com>
2021-02-21 23:10:34 +0800

    page

--- resources/views/debt/index.blade.php
+++ resources/views/debt/index.blade.php
@@ -2,7 +2,40 @@
 
 
 @section('content')
-debt page here
-    
+    <div class="card card-custom">
+        <div class="card-header"> Today's debt (Date) </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <th>Title</th>
+                    <th>Amount</th>
+                    <th>Time</th>
+                    <th>Status</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Milo air</td>
+                        <td>RM45</td>
+                        <td>marin dulu</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-danger">
+                                Unpaid
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Corntoz</td>
+                        <td>RM0.20</td>
+                        <td>Tadi jah baru</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-success">
+                                Paid
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
 
